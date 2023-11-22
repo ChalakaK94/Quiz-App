6465335f5107a7c7f7af9f3c4bea7a6989ddc189
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-22 06:50:39 +0530

    load quiz data from local server and add to reducer using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Header from './components/header';
+import Loader from './components/Loader';
 
+const initalState = {
+  questions:[],
+  status:'loading'
+}
+
+function reducer(state,action){
+  switch (action.type) {
+    case 'dataReceived':
+      return {...state, questions: action.payload, status:'ready'}
+      
+    case 'dataFailed':
+      return {...state, status:'error'}
+  }
+}
 function App() {
+
+   const [{status, questions}, dispatch ] = useReducer(reducer, initalState)
+
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+    .then(res => res.json())
+    .then(data=>dispatch({type:'dataReceived', payload:data }))
+    .catch(err=> dispatch({type:'dataFailed'}))
+  }, []);
+
   return (
     <div className="App">
       <div className='container'>
           <Header/> 
-          <Main/>
+          <Main> 
+            {status === 'loading' && <Loader/>}
+          </Main>
       </div>
     </div>
   );
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader(){
+    return <div className="loading">
+               <span className="loader"></span>
+               <div>Loading...</div>
+        
+    </div>
+}
